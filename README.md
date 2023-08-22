acaaa0fdb49b9af180344eeb6a8ba182e8ccb1cc
UmaruMG <49103742+UmaruMG@users.noreply.github.com>
2023-08-22 23:33:10 +0200

    Initialize new Project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Sims XML Auto Translator
---
+++ Sims XML Auto Translator/Form1.cs
@@ -0,0 +1,10 @@
+namespace Sims_XML_Auto_Translator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
