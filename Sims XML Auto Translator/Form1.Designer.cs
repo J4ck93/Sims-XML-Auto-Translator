c02edc6b1e527765152a533e6491f06dc508580a
UmaruMG <49103742+UmaruMG@users.noreply.github.com>
2023-08-23 01:43:20 +0200

    Added Basic UI, Added Code for the ComboBoxes, add Settings

--- Sims XML Auto Translator/Form1.Designer.cs
+++ Sims XML Auto Translator/Form1.Designer.cs
@@ -28,12 +28,216 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            importXMLToolStripMenuItem = new ToolStripMenuItem();
+            exportXMLToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem1 = new ToolStripSeparator();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            optionsToolStripMenuItem = new ToolStripMenuItem();
+            checkUsageToolStripMenuItem = new ToolStripMenuItem();
+            settingsToolStripMenuItem = new ToolStripMenuItem();
+            helpToolStripMenuItem = new ToolStripMenuItem();
+            checkForUpdateToolStripMenuItem = new ToolStripMenuItem();
+            helpToolStripMenuItem1 = new ToolStripMenuItem();
+            toolStripMenuItem2 = new ToolStripSeparator();
+            aboutToolStripMenuItem = new ToolStripMenuItem();
+            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            cbSourceLanguage = new ComboBox();
+            cbTargetLanguage = new ComboBox();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(19, 34);
+            button1.Name = "button1";
+            button1.Size = new Size(102, 32);
+            button1.TabIndex = 0;
+            button1.Text = "Import XML";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, optionsToolStripMenuItem, helpToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(280, 24);
+            menuStrip1.TabIndex = 4;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { importXMLToolStripMenuItem, exportXMLToolStripMenuItem, toolStripMenuItem1, exitToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // importXMLToolStripMenuItem
+            // 
+            importXMLToolStripMenuItem.Name = "importXMLToolStripMenuItem";
+            importXMLToolStripMenuItem.Size = new Size(137, 22);
+            importXMLToolStripMenuItem.Text = "Import XML";
+            // 
+            // exportXMLToolStripMenuItem
+            // 
+            exportXMLToolStripMenuItem.Name = "exportXMLToolStripMenuItem";
+            exportXMLToolStripMenuItem.Size = new Size(137, 22);
+            exportXMLToolStripMenuItem.Text = "Export XML";
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(134, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(137, 22);
+            exitToolStripMenuItem.Text = "Exit";
+            exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            // 
+            // optionsToolStripMenuItem
+            // 
+            optionsToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { checkUsageToolStripMenuItem, settingsToolStripMenuItem });
+            optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            optionsToolStripMenuItem.Size = new Size(61, 20);
+            optionsToolStripMenuItem.Text = "Options";
+            // 
+            // checkUsageToolStripMenuItem
+            // 
+            checkUsageToolStripMenuItem.Name = "checkUsageToolStripMenuItem";
+            checkUsageToolStripMenuItem.Size = new Size(142, 22);
+            checkUsageToolStripMenuItem.Text = "Check Usage";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            settingsToolStripMenuItem.Size = new Size(142, 22);
+            settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // helpToolStripMenuItem
+            // 
+            helpToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { checkForUpdateToolStripMenuItem, helpToolStripMenuItem1, toolStripMenuItem2, aboutToolStripMenuItem });
+            helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            helpToolStripMenuItem.Size = new Size(44, 20);
+            helpToolStripMenuItem.Text = "Help";
+            // 
+            // checkForUpdateToolStripMenuItem
+            // 
+            checkForUpdateToolStripMenuItem.Name = "checkForUpdateToolStripMenuItem";
+            checkForUpdateToolStripMenuItem.Size = new Size(166, 22);
+            checkForUpdateToolStripMenuItem.Text = "Check for Update";
+            // 
+            // helpToolStripMenuItem1
+            // 
+            helpToolStripMenuItem1.Name = "helpToolStripMenuItem1";
+            helpToolStripMenuItem1.Size = new Size(166, 22);
+            helpToolStripMenuItem1.Text = "Help";
+            // 
+            // toolStripMenuItem2
+            // 
+            toolStripMenuItem2.Name = "toolStripMenuItem2";
+            toolStripMenuItem2.Size = new Size(163, 6);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(166, 22);
+            aboutToolStripMenuItem.Text = "About";
+            // 
+            // button2
+            // 
+            button2.Enabled = false;
+            button2.Location = new Point(159, 34);
+            button2.Name = "button2";
+            button2.Size = new Size(102, 32);
+            button2.TabIndex = 2;
+            button2.Text = "Export XML";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(25, 82);
+            label1.Name = "label1";
+            label1.Size = new Size(90, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Input Language";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(160, 82);
+            label2.Name = "label2";
+            label2.Size = new Size(100, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Output Language";
+            // 
+            // cbSourceLanguage
+            // 
+            cbSourceLanguage.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbSourceLanguage.FormattingEnabled = true;
+            cbSourceLanguage.Location = new Point(22, 108);
+            cbSourceLanguage.Name = "cbSourceLanguage";
+            cbSourceLanguage.Size = new Size(96, 23);
+            cbSourceLanguage.TabIndex = 1;
+            // 
+            // cbTargetLanguage
+            // 
+            cbTargetLanguage.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbTargetLanguage.FormattingEnabled = true;
+            cbTargetLanguage.Location = new Point(162, 108);
+            cbTargetLanguage.Name = "cbTargetLanguage";
+            cbTargetLanguage.Size = new Size(96, 23);
+            cbTargetLanguage.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(280, 155);
+            Controls.Add(cbTargetLanguage);
+            Controls.Add(cbSourceLanguage);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(menuStrip1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            HelpButton = true;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Button button1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem importXMLToolStripMenuItem;
+        private ToolStripMenuItem exportXMLToolStripMenuItem;
+        private ToolStripSeparator toolStripMenuItem1;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem optionsToolStripMenuItem;
+        private ToolStripMenuItem checkUsageToolStripMenuItem;
+        private ToolStripMenuItem settingsToolStripMenuItem;
+        private ToolStripMenuItem helpToolStripMenuItem;
+        private ToolStripMenuItem checkForUpdateToolStripMenuItem;
+        private ToolStripMenuItem helpToolStripMenuItem1;
+        private ToolStripSeparator toolStripMenuItem2;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private ComboBox cbSourceLanguage;
+        private ComboBox cbTargetLanguage;
     }
 }
--- Sims XML Auto Translator/Form1.cs
+++ Sims XML Auto Translator/Form1.cs
@@ -1,3 +1,7 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http.Json;
+
 namespace Sims_XML_Auto_Translator
 {
     public partial class Form1 : Form
@@ -6,5 +10,67 @@ namespace Sims_XML_Auto_Translator
         {
             InitializeComponent();
         }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                LanguageContainer languageContainer = JsonConvert.DeserializeObject<LanguageContainer>(Properties.Resources.languages);
+
+                Language[] sourceLanguages = CreateLanguageArray(languageContainer.SourceLanguage);
+                Language[] targetLanguages = CreateLanguageArray(languageContainer.TargetLanguage);
+
+                cbSourceLanguage.DataSource = sourceLanguages;
+                cbSourceLanguage.DisplayMember = "Name";
+                cbSourceLanguage.ValueMember = "Code";
+                cbSourceLanguage.SelectedValue = Properties.Settings.Default.sourceLanguage;
+
+                cbTargetLanguage.DataSource = targetLanguages;
+                cbTargetLanguage.DisplayMember = "Name";
+                cbTargetLanguage.ValueMember = "Code";
+                cbTargetLanguage.SelectedValue = Properties.Settings.Default.targetLanguage;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        static Language[] CreateLanguageArray(Dictionary<string, string> languageDictionary)
+        {
+            List<Language> languages = new List<Language>();
+
+            foreach (var kvp in languageDictionary)
+            {
+                Language language = new Language(kvp.Key, kvp.Value);
+                languages.Add(language);
+            }
+
+            return languages.ToArray();
+        }
+    }
+
+    class Language
+    {
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public Language(string code, string name)
+        {
+            Code = code;
+            Name = name;
+        }
+    }
+
+    class LanguageContainer
+    {
+        public Dictionary<string, string> SourceLanguage { get; set; }
+        public Dictionary<string, string> TargetLanguage { get; set; }
     }
 }
